e6b2e1e99c196836d0008c8ec92fc56e7d21efd4
dennis <sanjosedennis7593@gmail.com>
2024-03-16 17:09:30 +0800

    git: cache clear

--- out/preload/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const require$$0 = require("electron");
-var preload = {};
-const electron = require$$0;
-const { contextBridge, ipcRenderer } = electron;
-contextBridge.exposeInMainWorld("electronAPI", {
-  openFile: () => ipcRenderer.invoke("dialog:openFile")
-});
-module.exports = preload;
